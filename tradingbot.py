571838f3f903f9651f18a7e6d9bb4962d5eb507c
Ayo-Cyber <atunraseayomide@gmail.com>
2024-05-21 09:49:39 +0100

    tradingbot pipeline

--- tradingbot.py
+++ tradingbot.py
@@ -1,8 +1,10 @@
 from lumibot.brokers import Alpaca
 from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies.strategy import Strategy
-from lumibot.trader import Trader
+from lumibot.traders import Trader
 from datetime import datetime
+from alpaca_trade_api.rest import REST
+from timedelta import Timedelta
 
 API_KEY = "PKKGCTWU6361MKOTJ789"
 API_SECRET = "WJrZ4Z1LQ5E0NvEeyq5pX1Qp2BSHErv43q74EKyC"
@@ -15,23 +17,59 @@ ALPACA_CREDENTIALS = {
 }
 
 class MLTrader(Strategy):
-    def initialize(self,symbol:str="SPY"):
+    def initialize(self,symbol:str="SPY",cash_at_risk:float=.5):
         self.symbol = symbol
         self.sleeptime = "24H"
+        self.last_trade = None
+        self.cash_at_risk = cash_at_risk
+        self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
+
+    def position_sizing(self):
+        cash = self.get_cash()
+        last_price = self.get_last_price(self.symbol)
+        quantity = round(cash * self.cash_at_risk / last_price)
+        return cash, last_price, quantity
+    
+    def get_dates(self):
+        today = self.get_datetime()
+        days_prior = today - Timedelta(days=5)
+        return today.strftime("%Y-%m-%d"), days_prior.strftime("%Y-%m-%d")
+
+    def get_news(self):
+        today, days_prior = self.get_dates()
+        news = self.api.get_news(self.symbol, start=days_prior, end=today)
+        news = [ev.__dict__["_raw"]["headline"] for ev in news]
+        return news
 
     def on_trading_iteration(self):
-        pass
+        cash, last_price, quantity = self.position_sizing()
+
+        if cash > last_price:
+            if self.last_trade == None:
+                news = self.get_news()
+                print(news)
+                order = self.create_order(
+                    self.symbol,
+                    quantity,
+                    "buy",
+                    type = "bracket",
+                    take_profit_price=last_price*1.20,
+                    stop_loss_price=last_price*.95
+                )
+                self.submit_order(order)
+                self.last_trade = "buy"
 
 start_date = datetime(2023,12,15)
 end_date = datetime(2023,12,31)
 
 broker = Alpaca(ALPACA_CREDENTIALS)
-strategy = MLTrader(name="ml_trader", broker=broker, parameters={symbol="SPY"})
+strategy = MLTrader(name = "ml_trader", broker = broker,
+                     parameters = {"symbol":"SPY" , "cash_at_risk":.5})
 
-startegy.backtest(
+strategy.backtest(
     YahooDataBacktesting,
     start_date,
     end_date,
-    parameters={symbol="SPY"}
+    parameters={"symbol":"SPY", "cash_at_risk":.5}
 )
 
